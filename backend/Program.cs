7b32de03a585f2495f6642f33dc69ae1c6a3c2de
sxha092024 <sxha092024@gmail.com>
2024-12-09 13:24:47 +0100

    init backend project (#1)
    * init backend project
    
    Provide the bare-bones scaffolding for a REST api in C# using AspNetCore with serilog for structured logging
    
    ---------
    
    Co-authored-by: sxha092024 <183362499+sxha092024@users.noreply.github.com>

---
+++ backend/Program.cs
@@ -0,0 +1,73 @@
+using System.Net;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.Async(a =>
+        a.File(
+            new JsonFormatter(renderMessage: true),
+            // Properely deliniate logfiles with a basename and a timestamp
+            "logs/rest_api_.log",
+            rollingInterval: RollingInterval.Day
+        )
+    )
+    .CreateLogger();
+try
+{
+    Log.Information("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddSerilog();
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddRouting(options =>
+    {
+        options.LowercaseUrls = true;
+    });
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc(
+            "v1",
+            new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Celebration booking API",
+                Description = "Providing an API for booking celebrations with other families",
+            }
+        );
+    });
+
+    var app = builder.Build();
+    // wwwroot
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+
+    if (app.Environment.IsDevelopment())
+    {
+        Log.Debug("Develop environment, enabling Swapper & OpenAPI");
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = "swagger";
+        });
+    }
+
+    app.MapControllers();
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
