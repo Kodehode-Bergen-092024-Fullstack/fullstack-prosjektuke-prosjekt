96b148431273ef01b257a494387e7bf6e86f83ca
sxha092024 <183362499+sxha092024@users.noreply.github.com>
2024-12-11 12:03:03 +0100

    Include additional documentation in openapi/swagger

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -9,14 +9,29 @@ public class ApiController : ControllerBase
     private readonly DataContext _dataContext;
     private readonly ILogger<ApiController> _logger;
 
+    /// <summary>
+    /// Returns all registered family objects
+    /// </summary>
+    /// <returns>An array of Family objects wrapped inside an IActionResult</returns>
+    /// <response code="200">Returns the located objects</response>
     [HttpGet("family/all")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult GetFamilies()
     {
         _logger.LogInformation("");
-        return Ok(new { Families = _dataContext.AsEnumerable().ToList() });
+        return Ok(_dataContext.AsEnumerable().ToList());
     }
 
+    /// <summary>
+    /// Get a registered family object by ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>A family object if it exists, otherwise not found</returns>
+    /// <response code="200">Returns the located object</response>
+    /// <response code="400">If the object is not registered</response>
     [HttpGet("family/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult GetFamily(int id)
     {
         var family = _dataContext.Get(id);
@@ -27,11 +42,20 @@ public class ApiController : ControllerBase
         }
         else
         {
-            return NotFound(new { Id = id });
+            return BadRequest(new { Id = id });
         }
     }
 
+    /// <summary>
+    /// Registers a new family object
+    /// </summary>
+    /// <param name="family"></param>
+    /// <returns>The newly registered object</returns>
+    /// <response code="200">Returns the created object</response>
+    /// <response code="400">If the object could not be registered due to conflicts</response>
     [HttpPost("family")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult CreateFamily([FromBody] Family family)
     {
         bool added = _dataContext.Add(family);
@@ -45,7 +69,16 @@ public class ApiController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Deletes a registered family object
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Success status</returns>
+    /// <response code="200">The object has been deleted</response>
+    /// <response code="400">The object was not deleted</response>
     [HttpDelete("family/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult DeleteFamily(int id)
     {
         bool deleted = _dataContext.Remove(id);
@@ -59,7 +92,17 @@ public class ApiController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Replaces a registered family object
+    /// </summary>
+    /// <param name="id">Must exist</param>
+    /// <param name="family">Cannot be a partial object</param>
+    /// <returns>Success status</returns>
+    /// <response code="200">The object has been replaced</response>
+    /// <response code="400">The object could not be replaced</response>
     [HttpPut("family/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult ReplaceFamily(int id, [FromBody] Family family)
     {
         var replaced = _dataContext.Update(id, family);
